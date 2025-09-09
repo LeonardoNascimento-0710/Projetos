90f1203e9ed2ab14ed3d3ca2d82b0e47fc64f3c2
Leonardo Nascimento <222035894+LeonardoNascimento-0710@users.noreply.github.com>
2025-09-09 11:12:35 -0300

    testes

---
+++ html-css-js/Testes/script.js
@@ -0,0 +1,15 @@
+const pedra = document.getElementById("pedra")
+const papel = document.getElementById("papel")
+const tesoura = document.getElementById("tesoura")
+
+let escolha = "";
+
+const botoes = document.querySelectorAll(".botao")
+
+pedra.addEventListener("click", function(jogar){
+
+})
+
+function jogar(){
+
+}
