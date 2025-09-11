dff17af6438fabc9fb3161ea298677171aed1dfa
Leonardo Nascimento <222035894+LeonardoNascimento-0710@users.noreply.github.com>
2025-09-11 17:39:34 -0300

    Jokenpo!

--- html-css-js/Testes/script.js
+++ html-css-js/Testes/script.js
@@ -6,10 +6,60 @@ let escolha = "";
 
 const botoes = document.querySelectorAll(".botao")
 
-pedra.addEventListener("click", function(jogar){
+pedra.addEventListener("click", function () {
+    let adversario = ["pedra", "papel", "tesoura"];
+    let indice = Math.floor(Math.random() * adversario.length);
+    let escolhaAdversario = adversario[indice];
 
+    let escolha = "pedra";
+
+    if (escolha === "pedra") {
+        if (escolhaAdversario === "pedra") {
+            alert("Empate!");
+        } else if (escolhaAdversario === "papel") {
+            alert("Você perdeu!");
+        } else if (escolhaAdversario === "tesoura") {
+            alert("Você ganhou!");
+        }
+    }
+});
+papel.addEventListener("click", function () {
+    let adversario = ["pedra", "papel", "tesoura"];
+    let indice = Math.floor(Math.random() * adversario.length);
+    let escolhaAdversario = adversario[indice];
+
+    let escolha = "papel";
+
+    if (escolha === "papel") {
+        if (escolhaAdversario === "pedra") {
+            alert("Você ganhou!")
+        }
+        else if (escolhaAdversario === "papel"){
+            alert("Empate!")
+        }
+        else if (escolhaAdversario === "tesoura"){
+            alert("Você Perdeu!")
+        }
+    }
+})
+tesoura.addEventListener("click", function() {
+    let adversario = ["pedra", "papel", "tesoura"];
+    let indice = Math.floor(Math.random() * adversario.length);
+    let escolhaAdversario = adversario[indice];
+
+    let escolha = "tesoura";
+
+    if (escolha === "tesoura") {
+        if (escolhaAdversario === "pedra") {
+            alert("Você Perdeu!")
+        }
+        else if (escolhaAdversario === "papel"){
+            alert("Você ganhou!")
+        }
+        else if (escolhaAdversario === "tesoura"){
+            alert("Empate!")
+        }
+    }
 })
 
-function jogar(){
 
-}
